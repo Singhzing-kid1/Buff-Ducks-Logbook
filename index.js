9d2edd20572b35aa264f84d8a8b75166c76ffbbb
Singhzing-kid1 <virsheeng@protonmail.com>
2023-05-24 21:06:58 -0600

    it works

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 function createLogEntryCardFormJson(cardData){
-    var template = [
-        '<div class="logEntryCard">',
-            '<div>',
-                '<img class="thumbnail" src="' + cardData.thumbnail || 'https://placehold.co/127x127/000/FFF/png" alt="thumbnail"/>',
-                '<button>Open Log</button>',
-            '</div>',
-            '<div>',
-                '<h1>' + cardData.author || 'Author Name' + '</h1>',
-                '<h2>' + cardData.date || 'date(dd/mm/yyyy)' + '</h2>',
-                '<p>' + cardData.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' + '</p>',
-            '</div>',
-        '</div>'
-    ];
-    return $(template.join(''));
-}
+    console.log(cardData);
 
-var listOfItems;
+    var template = `
+        <div class="logEntryCard">
+            <div>
+                <img class="thumbnail" src="${cardData.thumbnail || 'https://placehold.co/127x127/000/FFF.png'}" alt="thumbnail">
+                <button id="${cardData.id}">Open Log</button>
+            </div>
+            <div>
+                <h1>${cardData.author || 'Author Name'}</h1>
+                <h2>${cardData.date || 'date(dd/mm/yyyy)'}</h2>
+                <p>${cardData.description || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}</p>
+            </div>
+        </div>
+    `;
 
-$.getJSON('data.json', function(data){
-    listOfItems = data;
+    return $(template);}
+
+var cardList = [];
+
+$.ajax({
+    url: 'logEntries.json',
+    dataType: 'json',
+    success: function(data) {
+        var jsonData = data;
+
+        console.log(jsonData);
+
+        $.each(jsonData, function(index, cardData){
+            console.log(cardData);
+            cardList.push(cardData);
+        });
+
+
+        cardTemplates = $();
+
+        $.each(cardList, function(index, cardListItems){
+            console.log(cardListItems);
+            cardTemplates = cardTemplates.add(createLogEntryCardFormJson(cardListItems));
+        });
+
+        console.log(cardTemplates);
+
+
+        cardTemplates.appendTo('div.gridContainer');
+    }
+
 });
 
-console.log(listOfItems);
 
-var cards = $();
-
-listOfItems.forEach(function(item, i){
-    cards = cards.add(createLogEntryCardFormJson(item));
-});
+// console.log(jsonData);
 
 
-$(function(){
-    $('.gridContainer').append(cards);
-});
